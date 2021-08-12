20d4cdf4df97ee4ee368a10ad0a0a03a390d8775
nam3886 <nphuongnam8@gmail.com>
2021-08-13 00:05:40 +0700

    fix deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS','on');
+        }
     }
 }
